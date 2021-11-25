3cdc3a2bb4f35e414d4aa871126163d4542eb09a
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-24 22:44:30 -0500

    Probando servicio http para consumir apis

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,22 +1,11 @@
 const aplicacion = angular.module("universidadApp", []);
 
-aplicacion.controller("productoCtrl", function () {
-  this.productos = [
-    { id: 1, nombre: "Salchipapa", precio: 503612 },
-    { id: 2, nombre: "Choripapa", precio: 503612 },
-  ];
-  this.cantidad = this.productos.length;
+aplicacion.controller("postController", function ($http) {
+  const URL = "https://jsonplaceholder.typicode.com/posts";
 
-  this.agregarProducto = function () {
-    if (this.nombre.trim() !== "" && this.precio != null) {
-      this.cantidad = this.productos.length + 1;
-      this.productos.push({
-        id: this.cantidad,
-        nombre: this.nombre,
-        precio: this.precio,
-      });
-      this.nombre = "";
-      this.precio = null;
-    }
-  };
+  this.posts = [];
+  $http.get(URL).then(resultado => {
+    console.log(resultado)
+    this.posts = resultado.data
+  });
 });
