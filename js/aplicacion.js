b8d53c79aee2653864ae9d32f12440a80f0bec9c
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-24 23:33:19 -0500

    Agregada la operacion de crear un registro en el apirest

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -2,10 +2,26 @@ const aplicacion = angular.module("universidadApp", []);
 
 aplicacion.controller("postController", function ($http) {
   const URL = "https://jsonplaceholder.typicode.com/posts";
+  this.post = {};
 
   this.posts = [];
-  $http.get(URL).then(resultado => {
-    console.log(resultado)
-    this.posts = resultado.data
+  $http.get(URL).then((resultado) => {
+    this.posts = resultado.data;
+    this.cantidad = this.posts.length;
   });
+
+  this.addPost = function () {
+    $http
+      .post(URL, {
+        title: this.post.title,
+        body: this.post.body,
+        userId: 1,
+      })
+      .then((resultado) => {
+        console.log(this.post)
+        this.posts.push(resultado.data);
+        this.post = {};
+      })
+      .catch((error) => console.log(error));
+  };
 });
